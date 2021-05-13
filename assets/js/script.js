f1a07edd224300ba0a7aff1520901fe43338a00c
Barbara Pereira <bpereira74@gmail.com>
2021-05-13 08:58:10 -0400

    inserción typeform

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 
 $(function () {
     $('[data-bs-toggle="popover"]').popover()
-    });
+});
 
 
-    $("li").click(function(click){
-     alert(this.hash)
+$("a").click(function(){
+     alert("hola")
     
 });
-});
+
+
+
+//Inicialización Popover
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
